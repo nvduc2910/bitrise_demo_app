82e341bc8d30adaa0d914ad6cb57970ac4c342fd
Duckie N <nvduc2910@gmail.com>
2020-02-24 11:17:47 +0700

    init project

---
+++ bitrise_demo_app/bitrise_demo_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitrise_demo_app
+//
+//  Created by Duckie N on 2/24/20.
+//  Copyright © 2020 Nguyen Van Duc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bitrise_demo_app/bitrise_demo_appTests/bitrise_demo_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  bitrise_demo_appTests.swift
+//  bitrise_demo_appTests
+//
+//  Created by Duckie N on 2/24/20.
+//  Copyright © 2020 Nguyen Van Duc. All rights reserved.
+//
+
+import XCTest
+@testable import bitrise_demo_app
+
+class bitrise_demo_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
